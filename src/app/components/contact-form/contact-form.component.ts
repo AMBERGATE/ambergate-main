81920a2bba7cfd5816a604f85254f7a05be1ffe6
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-06-23 22:53:16 -0300

    chore

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
 import { MaterialModule } from '../../shared/material/material.module';
 
 @Component({
@@ -13,19 +12,4 @@ import { MaterialModule } from '../../shared/material/material.module';
 })
 export class ContactFormComponent {
   constructor() {}
-
-  onSubmit(contactForm?: NgForm) {
-    const formValues = contactForm?.value;
-
-    emailjs
-      .send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formValues, 'YOUR_USER_ID')
-      .then(
-        (result: EmailJSResponseStatus) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  }
 }
