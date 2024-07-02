0c2cf449eab434745804ad885194b0ca16b9063a
Luca-Mendoza-Coder <100142127+Luca-Mendoza-Coder@users.noreply.github.com>
2024-07-02 12:32:10 -0300

    fixer(): url img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,49 +25,49 @@ export class AppComponent {
 
   interior_selective_demolitions = [
     {
-      url: './../assets/img/1-interior-selective-demolitions/01.webp',
+      url: './assets/img/1-interior-selective-demolitions/01.webp',
     },
     {
-      url: './../assets/img/1-interior-selective-demolitions/03.webp',
+      url: './assets/img/1-interior-selective-demolitions/03.webp',
     },
     {
-      url: './../assets/img/1-interior-selective-demolitions/04.webp',
+      url: './assets/img/1-interior-selective-demolitions/04.webp',
     },
   ];
 
   HIGH_PRESSURE_CLEANING = [
     {
-      url: './../assets/img/3-high-pressure/01.webp',
+      url: './assets/img/3-high-pressure/01.webp',
     },
     {
-      url: './../assets/img/3-high-pressure/02.webp',
+      url: './assets/img/3-high-pressure/02.webp',
     },
     {
-      url: './../assets/img/3-high-pressure/03.webp',
+      url: './assets/img/3-high-pressure/03.webp',
     },
   ];
 
   FLOORING_DEMOLITION_REMOVAL = [
     {
-      url: './../assets/img/2-flooring-demolition-removal/01.webp',
+      url: './assets/img/2-flooring-demolition-removal/01.webp',
     },
     {
-      url: './../assets/img/2-flooring-demolition-removal/02.webp',
+      url: './assets/img/2-flooring-demolition-removal/02.webp',
     },
   ];
 
   JUNK_REMOVAL = [
     {
-      url: './../assets/img/4-junk-removal/01.webp',
+      url: './assets/img/4-junk-removal/01.webp',
     },
     {
-      url: './../assets/img/4-junk-removal/02.webp',
+      url: './assets/img/4-junk-removal/02.webp',
     },
     {
-      url: './../assets/img/4-junk-removal/03.webp',
+      url: './assets/img/4-junk-removal/03.webp',
     },
     {
-      url: './../assets/img/4-junk-removal/04.webp',
+      url: './assets/img/4-junk-removal/04.webp',
     },
   ];
 }
